ddaedb8d7be6c2df9a356795f23d3997f6261193
Danny Adams <danny@initek.com>
2018-03-18 17:43:41 -0700

    Added Wait

--- Code/Assets/Scripts/Ball.cs
+++ Code/Assets/Scripts/Ball.cs
@@ -6,29 +6,34 @@ public class Ball : MonoBehaviour {
 
     float speed = 4f;
     Rigidbody2D rigid;
-
+    bool isIdle = true;
 
     // Use this for initialization
     private void Start()
     {
-        StartBall();
+        rigid = GetComponent<Rigidbody2D>();
+        StartCoroutine(StartBall());
     }
 
-    void StartBall () {
+    IEnumerator StartBall () {
+        isIdle = true;
+        yield return new WaitForSeconds( 3 );
         transform.position = Vector3.zero;
         float randomY = Random.Range(-speed, speed);
         float xNegPos = Random.Range(0f, 1f);
         if (xNegPos <= 0.5f) xNegPos = -1;
         else xNegPos = 1;
-        rigid = GetComponent<Rigidbody2D>();
         rigid.velocity = new Vector2(speed * xNegPos, randomY);
+        isIdle = false;
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        if ( isIdle ) return;
         if (Mathf.Abs(rigid.velocity.y) <= 1f) rigid.velocity = new Vector2(rigid.velocity.x, speed * 0.5f);
         if (Mathf.Abs(rigid.velocity.x) <= 1f) rigid.velocity = new Vector2(speed * 0.5f, rigid.velocity.y);
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         string colName = collision.name;
@@ -42,6 +47,7 @@ public class Ball : MonoBehaviour {
             print("player2 Scored!");
             ScoreKeeper.Instance.Score(false);
         }
-        StartBall();
+        StartCoroutine( StartBall() );
     }
+
 }
